e3a8ddf7ad6c64dda9a85cacac0270f58c7e7fde
parvez mosharrof <pk9435497@gmail.com>
2024-10-22 12:49:00 -0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        "primary": "#0092E4;",
+        "secondary": "#183656;"
+      }
+    },
+  },
+  plugins: [],
+}
+
